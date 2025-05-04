b32f26e868a98789d0a63629173f17f364c8d232
nahal <mrym.mhdzde@gmail.com>
2025-05-04 20:53:33 +0330

    Add POST /products route with in-memory product creation

--- src/models/product.rs
+++ src/models/product.rs
@@ -1,8 +1,15 @@
-use serde::Serialize;
+use serde::{Serialize,Deserialize};
 
 #[derive(Serialize)]
+#[derive(Clone)]
 pub struct Product {
     pub id: u32,
+    pub name: String,
+    pub price: f64,
+}
+
+#[derive(Deserialize)]
+pub struct ProductInput {
     pub name: String,
     pub price: f64,
 }
--- src/routes/products.rs
+++ src/routes/products.rs
@@ -1,23 +1,39 @@
-use axum::{routing::get, Json, Router};
-use crate::models::product::Product;
+use axum::{
+    Json, Router,
+    extract::State,
+    routing::get,
+};
+use std::sync::{Arc, Mutex};
+
+use crate::models::product::{Product, ProductInput};
+
+type ProductList = Arc<Mutex<Vec<Product>>>;
 
 pub fn product_routes() -> Router {
-    Router::new().route("/products", get(get_products))
+    let products = Arc::new(Mutex::new(vec![])); // shared state
+    Router::new()
+        .route("/products", get(get_products).post(create_product))
+        .with_state(products)
 }
 
-async fn get_products() -> Json<Vec<Product>> {
-    let products = vec![
-        Product {
-            id: 1,
-            name: "Shoes".to_string(),
-            price: 79.99,
-        },
-        Product {
-            id: 2,
-            name: "Bag".to_string(),
-            price: 45.50,
-        },
-    ];
+async fn get_products(State(products): State<ProductList>) -> Json<Vec<Product>> {
+    let data = products.lock().unwrap();
+    Json(data.clone())
+}
 
-    Json(products)
+async fn create_product(
+    State(products): State<ProductList>,
+    Json(payload): Json<ProductInput>,
+) -> Json<Product> {
+    let mut data = products.lock().unwrap();
+    let new_id = (data.len() + 1) as u32;
+
+    let new_product = Product {
+        id: new_id,
+        name: payload.name,
+        price: payload.price,
+    };
+
+    data.push(new_product.clone());
+    Json(new_product)
 }
